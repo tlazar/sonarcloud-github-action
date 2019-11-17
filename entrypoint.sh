a9ec98f51b233e45b609afdca7304aa0f44beb54
Toby Lazar <tlazar@capitaltg.com>
2019-11-17 18:48:25 -0500

    why can't gradle users use this action?

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,6 @@ if [[ -f "pom.xml" ]]; then
   exit 1
 fi
 
-if [[ -f "build.gradle" ]]; then
-  echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
-  exit 1
-fi
-
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
   EVENT_ACTION=$(jq -r ".action" "${GITHUB_EVENT_PATH}")
   if [[ "${EVENT_ACTION}" != "opened" ]]; then
